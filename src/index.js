47a206d826ea1b74fab826337aeb951ced3f9c91
mohammadjalizi <mjalizi1376@gmail.com>
2023-04-29 16:26:17 +0330

    using react router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
  
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route index="/" element={<Home />}>
+      {/* <Route path="dashboard" element={< />} /> */}
+      {/* ... etc. */}
+    </Route>
+  )
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
