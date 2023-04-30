09f67ffa19ae8b6dd711645fbdd4967dfe73fe28
mohammadjalizi <mjalizi1376@gmail.com>
2023-04-30 12:47:20 +0330

    using react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import Home from "./pages/Home";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route index="/" element={<Home />}>
-      {/* <Route path="dashboard" element={< />} /> */}
-      {/* ... etc. */}
-    </Route>
-  )
-);
+
 
 function App() {
   return (
     <>
-       <RouterProvider router={router} />
-    <Home/>
+  
+
 </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,27 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
- 
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
   RouterProvider,
 } from "react-router-dom";
 import Home from './pages/Home';
+import Html from './pages/Html';
+import Css from './pages/Css';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route index="/" element={<Home />}>
-      {/* <Route path="dashboard" element={< />} /> */}
-      {/* ... etc. */}
-    </Route>
-  )
-);
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement:<h1>sory.....</h1>
+  },
+  {
+    path: "/html",
+    element: <Html/>,
+    errorElement:<h1>sory.....</h1>
+  },
+  {
+    path: "/css",
+    element: <Css/>,
+    errorElement:<h1>sory.....</h1>
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+
   </React.StrictMode>
 );
 
