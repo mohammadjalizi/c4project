5169e853a4d945bc16ccccd4d3b541c388bc4791
mohammadjalizi <mjalizi1376@gmail.com>
2023-05-09 21:09:12 +0330

    Show error message

--- src/pages/Java.js
+++ src/pages/Java.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
 import Header from '../Copm/Header'
 import Footer from '../Copm/Footer'
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from "../firebase/Config";
 const Java = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+useEffect(()=>{
+if(!user){
+
+navigate("/Signin")
+
+
+}
+
+
+
+})
   return (
     <>
 <Header/>
