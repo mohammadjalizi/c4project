76a9d84cd5f7a67e14b72ef4bfd7a700baa8f742
mohammadjalizi <mjalizi1376@gmail.com>
2023-05-09 20:58:47 +0330

    Show error message

--- src/pages/Html.js
+++ src/pages/Html.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../Copm/Header'
 import Maincontent from '../Copm/Maincontent'
 import Footer from '../Copm/Footer'
@@ -9,13 +9,17 @@ import { auth } from "../firebase/Config";
 const Html = () => {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
-  useEffect(() => {
-    if () {
-    
-      navigate("/session-timed-out");
-    }
-  });
+useEffect(()=>{
+if(!user){
+
+navigate("/Signin")
+
+
 }
+
+
+
+})
   return (
     <>
         <Helmet>
